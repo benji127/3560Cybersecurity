3f28b4c4f85c1d1f1649a12a63633076537efc57
benji127 <eneapaja27@gmail.com>
2023-03-03 13:36:49 -0800

    h2l

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -5,3 +5,4 @@
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
+print("Hello World")
