a826d588c87e9a6ee7967a6f6884184d159f897b
benji127 <eneapaja27@gmail.com>
2023-03-21 15:28:21 -0700

    Added new files subnet and functions

--- start/CH04/ScannerExample.py
+++ start/CH04/ScannerExample.py
@@ -1,5 +1,27 @@
 #!/usr/bin/env python3
 # Port scanner example 
 # Use 'pip3 install python-nmap' to install modules
-# Use 'sudo apt -y install nmap' to install nmapccc
-# By 
+# Use 'sudo apt -y install nmap' to install nmap
+# By Enea Paja
+# 2/27/2021
+
+# import necessary Python modules
+import nmap
+
+# Identify target address
+target_address = "10.0.2.15"
+
+# Identify start and stop port for the scan
+port_start = 20
+port_end = 30
+
+# Create the scanner object
+scanner = nmap.PortScanner()
+
+print("Scanning {0}".format(target_address))
+# Loop through each port and scan
+for port in range(port_start, port_end):
+    result = scanner.scan(target_address, str(port))
+    port_status = result['scan'][target_address]['tcp'][port]['state']
+    print("\tPort: {0} is {1}".format(port, port_status))
+
--- start/CH04/pinger6.py
+++ start/CH04/pinger6.py
@@ -1,4 +1,55 @@
 #!/usr/bin/env python3
 # Sixth example of pinging from Python
 # Writing log messages to a file
-# By 
+# By Enea Paja
+
+import platform
+import os
+from datetime import datetime
+
+
+def write_log(message):
+    now=str(datetime.now()) + "\t"
+    message = now + str(message) +"\n"
+    f = open("pinger.log", "a")
+    f.write(message)
+    f.close()
+
+def ping_host(ip):
+    
+    ping_cmd = f"ping -n 1 -w 2 {ip} > /dev/null 2>&1"
+
+    return ping_cmd
+
+def import_addresses():
+    # Create empty list object
+    lines =[]
+    # Open file and read line by line
+    f=open("ips.txt", "r")
+
+    for line in f:
+        # User strip() to remove spaces and carriage return
+        line = line.strip()
+        # Add the line to the lines list obj
+        lines.append(line)
+
+        # Return the list obj to the main body
+    return lines
+
+    #Read IPS from file
+
+write_log("Reading IPs from ips.txt")
+ip_addreses=import_addresses()
+write_log("Imported {0} IPs".format(len(ip_addreses)))
+
+for ip in ip_addreses:
+    #Call ping_host function and capture the return value
+    ping_cmd = ping_host(ip)
+
+    #Print results to onsole only if successful
+    if ping_cmd==0:
+        write_log("{0} is online".format(ip))
+        print("{0} is online".format(ip))
+    else:
+        write_log("{0} is offline ".format(ip))    
+    
