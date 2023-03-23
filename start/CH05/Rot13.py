546ea02f53f1e30711a0b2d2ba081bc5c67a5fd6
benji127 <eneapaja27@gmail.com>
2023-03-23 10:57:23 -0700

    Cryptography

--- start/CH05/Rot13.py
+++ start/CH05/Rot13.py
@@ -1,3 +1,23 @@
-#!/usr/bin/env python3
-# Script that encrypts/decrypts text using ROT13
-# By 
+#!/usr/bin/env/pythin3
+#Enea Paja
+#2023-03-23
+
+source_message=input("What is the maessage to encrypt ?")
+source_message = source_message.lower()
+final_message=""
+
+for letter in source_message:
+    ascii_num=ord(letter)
+
+    if ascii_num >= 97 and ascii_num <=122:
+        new_ascii = ascii_num +13
+
+        if new_ascii > 122:
+            new_ascii= new_ascii-26
+        final_message = final_message + chr(new_ascii)
+    else:
+        final_message = final_message + chr(ascii_num)
+
+print("Message has been converted") 
+
+print(final_message)
