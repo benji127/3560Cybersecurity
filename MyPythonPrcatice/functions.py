a4baaa10d7cf75bcdbd8efac9dd01980b9bcc23b
benji127 <eneapaja27@gmail.com>
2023-06-04 09:19:30 -0700

    Gith-ginore

--- MyPythonPrcatice/functions.py
+++ MyPythonPrcatice/functions.py
@@ -1,14 +1,16 @@
 #########################################
 #     Converting IP to number (decimal) #          
 #########################################
-# def ip_to_decimal(ip_address):
-#     # Split the IP address into its four octets (segments)
-#     octets = ip_address.split('.')
+def ip_to_decimal(ip_address):
+    # Split the IP address into its four octets (segments)
+    octets = ip_address.split('.')
     
-#     # Convert each octet to an integer and shift it to its proper position
-#     decimal = (int(octets[0]) << 24) + (int(octets[1]) << 16) + (int(octets[2]) << 8) + int(octets[3])
+    print(octets)
+    # Convert each octet to an integer and shift it to its proper position
+    decimal = (int(octets[0]) << 24) + (int(octets[1]) << 16) + (int(octets[2]) << 8) + int(octets[3])
     
-#     return decimal
+    print(decimal)
+    return decimal
 
 ##########################################
 #         USAGE OF THIS FUNCTION         #     
@@ -19,12 +21,30 @@
 # print(f"The IP converted into decimal is: {decimal}")  # Output: 3232235777
 # ___________________________________________________________________________________________________
 
-def ip_str_to_num(ip_str):
+def ip_to_decimal_2(ip_address):
     ip_num = 0
-    octet = ip_str.split(".")
+    octet = ip_address.split(".")
     for pow in range(3, -1, -1):
         ip_num += int(octet[3-pow]) * 256 ** pow
-    return ip_num
+    print(ip_num)
+    return ip_num   
 
-ip_str_to_num(input(""))
+def dec_to_ip(decimal):
+    octets = []
+    for i in range(4):
+        octet = int(decimal) % 256
+        octets.append(str(octet))
+        decimal//=256
+    print(".".join(octets))
+    return ".".join(octets)
+ 
+
+
+ip_num= input("What the IP ? ")
+# ip_to_decimal(ip_num)
+# ip_to_decimal_2(ip_num)
+dec_to_ip(ip_num)
+
+
+ 
 
