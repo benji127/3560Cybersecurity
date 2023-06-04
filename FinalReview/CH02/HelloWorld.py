5620bf60462d118e7df96b80db4b7fa299c06367
benji127 <eneapaja27@gmail.com>
2023-06-04 14:02:14 -0700

    Cached Gitignore

--- FinalReview/CH02/HelloWorld.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-#A simple "Hello"
-
-print("Hello")
-message= input("Who are you")
-
-# print("Hello" + message) 
-# print("Hello {0}".format(message))
-# print(f"Hello {message}")
--- FinalReview/Functions/HASHING/hash.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-#hash function
-def hash_epa_65(text_to_hash_epa_65):
-    h_epa_65 = hashlib.new(hash_name_epa_65)
-    h_epa_65.update(text_to_hash_epa_65.encode())
-
-    return h_epa_65.hexdigest()
